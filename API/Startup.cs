79c4382eb1975588dd61e1da67603420360a5bc4
puen_pepsi <puen_pepsi@hotmail.com>
2021-03-20 15:37:33 +0700

    PreContent

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,7 +34,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
-
+            services.AddCors();
         } 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +48,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 

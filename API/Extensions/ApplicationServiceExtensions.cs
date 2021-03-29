9e4a5c5a26c43f4733f210d4e571e0c86bab4a1d
puen_pepsi <puen_pepsi@hotmail.com>
2021-03-29 21:10:41 +0700

    End of Likes

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -15,6 +15,7 @@ namespace API.Extensions
             services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<ITokenService,TokenService>();
             services.AddScoped<IPhotoService,PhotoService>();
+            services.AddScoped<ILikesRepository,LIkesRepository>();
             services.AddScoped<LogUserActivity>();
             services.AddScoped<IUserRepository,UserRepository>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -91,6 +91,14 @@ export class MembersService {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 
+  addLike(username:string){
+    return this.http.post(this.baseUrl +'likes/' + username,{});
+  }
+  getLikes(predicate:string,pageNumber,pageSize){
+    let params = this.getPaginationHeaders(pageNumber,pageSize);
+    params = params.append('predicate',predicate);
+    return this.getPaginationedResult<Partial<Member[]>>(this.baseUrl + 'likes',params);
+  }
   private getPaginationedResult<T>(url, params) {
     const paginatedResult : PaginatedResult<T> = new PaginatedResult<T>();
 

625eeb26070871a96d1c160eb34f4b8fbc5cbf63
puen_pepsi <puen_pepsi@hotmail.com>
2021-07-02 10:44:12 +0700

    editback

--- API/Program.cs
+++ API/Program.cs
@@ -17,30 +17,30 @@ namespace API
 {
     public class Program
     {
-         public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-        // public static async Task Main(string[] args)
+        //  public static void Main(string[] args)
         // {
-        //     var host = CreateHostBuilder(args).Build();
-        //     using var scope = host.Services.CreateScope();
-        //     var services = scope.ServiceProvider;
-        //     try
-        //     {
-        //         var context = services.GetRequiredService<DataContext>();
-        //         var userManager = services.GetRequiredService<UserManager<AppUser>>();
-        //         var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
-        //         await context.Database.MigrateAsync();
-        //         await Seed.SeedUsers(userManager,roleManager);
-        //     }
-        //     catch(Exception ex)
-        //     {
-        //         var logger = services.GetRequiredService<ILogger<Program>>();
-        //         logger.LogError(ex,"An error occurred during migration");
-        //     }
-        //     await host.RunAsync();
+        //     CreateHostBuilder(args).Build().Run();
         // }
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            try
+            {
+                var context = services.GetRequiredService<DataContext>();
+                var userManager = services.GetRequiredService<UserManager<AppUser>>();
+                var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
+                await context.Database.MigrateAsync();
+                await Seed.SeedUsers(userManager,roleManager);
+            }
+            catch(Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex,"An error occurred during migration");
+            }
+            await host.RunAsync();
+        }
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>

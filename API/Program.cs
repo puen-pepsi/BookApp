fe426319027d7ac579dccbc7c1aa3b97d6e2a6bc
puen_pepsi <puen_pepsi@hotmail.com>
2021-07-02 16:05:03 +0700

    back123

--- API/Program.cs
+++ API/Program.cs
@@ -13,51 +13,39 @@ using Microsoft.Extensions.Logging;
 
 namespace API
 {
-    public class Program
-    {
-        //mark update
-    //     public static async Task Main(string[] args)
-    //     {
-    //         var host = CreateHostBuilder(args).Build();
-    //         using var scope = host.Services.CreateScope();
-    //         var services = scope.ServiceProvider;
-    //         try
-    //         {
-    //             var context = services.GetRequiredService<DataContext>();
-    //             var userManager = services.GetRequiredService<UserManager<AppUser>>();
-    //             var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
-    //             await context.Database.MigrateAsync();
-    //             await Seed.SeedUsers(userManager,roleManager);
-    //         }
-    //         catch(Exception ex)
-    //         {
-    //             var logger = services.GetRequiredService<ILogger<Program>>();
-    //             logger.LogError(ex,"An error occurred during migration");
-    //         }
-    //         await host.RunAsync();
-    //     }
-        
-    //     public static IHostBuilder CreateHostBuilder(string[] args) =>
-    //         Host.CreateDefaultBuilder(args)
-    //             .ConfigureWebHostDefaults(webBuilder =>
-    //             {
-    //                 webBuilder.UseStartup<Startup>();
-    //             });
-    // }
-       public static void Main(string[] args)
+        public class Program
         {
-        var config = new ConfigurationBuilder().AddCommandLine(args).Build();
-        var host = new WebHostBuilder()
-            .UseKestrel()
-            .UseContentRoot(Directory.GetCurrentDirectory())
-            .UseConfiguration(config)
-            .UseIISIntegration()
-            .UseStartup<Startup>()
-            .Build();
-
-        host.Run();
+            //mark update
+            public static async Task Main(string[] args)
+            {
+                var host = CreateHostBuilder(args).Build();
+                using var scope = host.Services.CreateScope();
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    var userManager = services.GetRequiredService<UserManager<AppUser>>();
+                    var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
+                    await context.Database.MigrateAsync();
+                    await Seed.SeedUsers(userManager,roleManager);
+                }
+                catch(Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex,"An error occurred during migration");
+                }
+                await host.RunAsync();
+            }
+            
+            public static IHostBuilder CreateHostBuilder(string[] args) =>
+                Host.CreateDefaultBuilder(args)
+                    .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                        webBuilder.UseStartup<Startup>();
+                    });
         }
-    }
+      
+    
 }
 
   
